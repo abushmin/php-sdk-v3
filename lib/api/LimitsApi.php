db2a5207f838c681196e63abda1efcf890f31280
abushmin <abushmin@users.noreply.github.com>
2022-02-21 11:40:56 +0300

    Update LimitsApi.php

--- lib/api/LimitsApi.php
+++ lib/api/LimitsApi.php
@@ -318,7 +318,7 @@ class LimitsApi
 
             } else {
                 // for HTTP post (form)
-                $httpBody = \GuzzleHttp\Psr7\build_query($formParams);
+                $httpBody = \GuzzleHttp\Psr7\Query::build($formParams);
             }
         }
 
@@ -339,7 +339,7 @@ class LimitsApi
             $headers
         );
 
-        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        $httpBody = \GuzzleHttp\Psr7\Query::build($formParams);
         return new Request(
             'GET',
             $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
